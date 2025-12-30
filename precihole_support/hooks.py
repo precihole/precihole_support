af1e95145cc3360df3c8468ca3327fa2f693960e
SanaKhan2000 <ksana@preciholesports.com>
2025-12-30 14:31:50 +0530

    did changes in hooks

--- precihole_support/hooks.py
+++ precihole_support/hooks.py
@@ -125,9 +125,6 @@ app_license = "mit"
 doc_events = {
     "Delivery Note": {
         "after_submit": "precihole_support.precihole_support.public.py.delivery_note.create_commissioning"
-    },
-    "Service Report": {
-        "validate": "precihole_support.precihole_support.doctype.service.service.Service.validate"
     }
 }
 
