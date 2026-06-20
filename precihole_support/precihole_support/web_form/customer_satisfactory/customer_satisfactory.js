b005dfb18758d72aa2e1fb64cb0ea6b1d36d46bc
SanaKhan2000 <ksana@preciholesports.com>
2026-06-20 15:46:33 +0530

    changes

--- precihole_support/precihole_support/web_form/customer_satisfactory/customer_satisfactory.js
+++ precihole_support/precihole_support/web_form/customer_satisfactory/customer_satisfactory.js
@@ -13,7 +13,7 @@ frappe.ready(function() {
                         let commissioning = r.message;
                         console.log(r.message);
 
-                        // Auto-fill parent fields properly
+                        // Auto-fill parent fields 
                         frappe.web_form.set_value("customer", commissioning.customer);
                         frappe.web_form.set_value("location", commissioning.location);
 
