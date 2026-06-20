ac4f206c8f2c111a72229964e24aa532c9570394
SanaKhan2000 <ksana@preciholesports.com>
2026-06-20 10:57:49 +0530

    remark needed in comissioning

--- precihole_support/precihole_support/doctype/commissioning/commissioning.py
+++ precihole_support/precihole_support/doctype/commissioning/commissioning.py
@@ -8,29 +8,36 @@ from frappe.utils import add_years, add_days
 
 
 class Commissioning(Document):
-	def before_submit(self):
-		for task in self.work_done:
-			if not task.out_comes:
-				frappe.throw('Out comes is mandatory')
-		docstatus = frappe.db.get_value('Training Sheet', {'commissioning': self.name}, 'docstatus')
-		if docstatus == None:
-			frappe.throw('No training sheet found')
-		if docstatus != 1:
-			
-			frappe.throw('Either Training Sheet is in Draft or Cancelled')
 
-			if not self.warranty_start_date:
-					frappe.throw("Warranty Start Date is required")
+    def before_submit(self):
+        for task in self.work_done:
+            if not task.out_comes:
+                frappe.throw("Out comes is mandatory")
 
-			self.remark = (
-				"The Above mentioned machine has been commissioned satisfactorily on "
-				+ formatdate(self.warranty_start_date, "dd-MM-yyyy")
-				+ " and handed over to Production Department"
-			)
-	
-	
-		
-	
-	def validate(doc, method=None):
-		if doc.warranty_start_date:
-			doc.warranty_end_date = add_days(add_years(doc.warranty_start_date, 1), -1)	
+        docstatus = frappe.db.get_value(
+            "Training Sheet",
+            {"commissioning": self.name},
+            "docstatus"
+        )
+
+        if docstatus is None:
+            frappe.throw("No training sheet found")
+
+        if docstatus != 1:
+            frappe.throw("Either Training Sheet is in Draft or Cancelled")
+
+        if not self.warranty_start_date:
+            frappe.throw("Warranty Start Date is required")
+
+        self.remark = (
+            "The above mentioned machine has been commissioned satisfactorily on "
+            + formatdate(self.warranty_start_date, "dd-MM-yyyy")
+            + " and handed over to Production Department"
+        )
+
+    def validate(self):
+        if self.warranty_start_date:
+            self.warranty_end_date = add_days(
+                add_years(self.warranty_start_date, 1),
+                -1
+            )
