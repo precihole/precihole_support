a4ed6dc399bfaa1e1d9d6752673cf259d4974097
SanaKhan2000 <ksana@preciholesports.com>
2026-09-24 16:09:07 +0530

    updates

--- precihole_support/precihole_support/doctype/service/service.py
+++ precihole_support/precihole_support/doctype/service/service.py
@@ -5,40 +5,7 @@ import frappe
 from frappe.model.document import Document
 
 
-# class Service(Document):
-	
-# 	def validate(self):
-#         # Run commissioning fetch logic before saving
-# 		self.set_commissioning_details()
+class Service(Document):
+    pass
 
-# 	def set_commissioning_details(self):
-# 		if self.machine_no:
-# 				commissioning = frappe.get_all(
-# 					"Commissioning",
-# 					filters={"machine_no": self.machine_no, "docstatus": 1},  # only submitted records
-# 					fields=[
-# 						"name",
-# 						"customer",
-# 						"location",
-# 						"machine_no",
-# 						"machine_type",
-# 						"posting_date",
-# 						"warranty_start_date",
-# 						"warranty_end_date"
-# 					]
-# 				)
-
-# 				if commissioning:
-# 					data = commissioning[0]
-# 					# link back to commissioning record
-# 					self.commissioning_no = data.name
-# 					self.customer = data.customer
-# 					self.location = data.location
-# 					self.machine_no = data.machine_no
-# 					self.machine_type = data.machine_type
-
-# 					self.warranty_start_date = data.warranty_start_date
-# 					self.warranty_end_date = data.warranty_end_date
-# 					self.dispatch_date = data.posting_date
-# 					self.commissioning_date = data.warranty_start_date
 				
