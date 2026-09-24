95675f1507e478a053ef5259875f63be47e4c6fa
SanaKhan2000 <ksana@preciholesports.com>
2026-09-24 16:03:02 +0530

    changes in py file for service

--- precihole_support/precihole_support/doctype/service/service.py
+++ precihole_support/precihole_support/doctype/service/service.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2025, Precihole Sports Pvt Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
-# from frappe.model.document import Document
+import frappe
+from frappe.model.document import Document
 
 
 # class Service(Document):
